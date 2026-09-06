b01f341fb01163df7270b557e9851a4f7c008170
Akshay-0614 <doddiakshaykumar2@gmail.com>
2026-09-06 13:18:33 +0530

    This Program can find the given number is Even or Odd.

---
+++ src/com/task/EvenOdd.java
@@ -0,0 +1,15 @@
+package com.task;
+
+public class EvenOdd {
+		static int x=5;
+	public static void main(String[] args) {
+		     new EvenOdd();
+		if(x/2==0) {
+			System.out.println("Given number is Even");
+		}
+		else {
+			System.out.println("Give number is Odd");
+		}
+	}
+
+}
