2a09efbb4ef84caba11871f7fea712c0cbabf2ad
Akshay-0614 <doddiakshaykumar2@gmail.com>
2026-09-13 22:07:57 +0530

    Add even or odd program
    Checks whether a given number is even or odd using the modulus operator and if-else statement.

---
+++ 01-Java-Basics/EvenOrOdd.java
@@ -0,0 +1,33 @@
+package corejava;
+
+public class EvenOrOdd {
+	
+	//Instance variable to store value
+	int a;
+
+	public static void main(String[] args) {
+		
+		System.out.println("Check the given Number is Even or Odd ");
+		System.out.println(" ");
+
+		
+		//creating an object of the EvenOrOdd class
+		EvenOrOdd e= new EvenOrOdd();
+		e.a=20;
+		
+		//Checking the given number is even or odd
+		//If the Given number is even,the if block will execute
+		
+		if (e.a % 2 == 0) { //checking the number with condition
+			
+			System.out.println("The given number is Even : "+e.a);
+		
+			}
+		//If the given number is odd, the else block will execute
+		else{
+			System.out.println("The given Number is Odd : "+e.a);
+		}
+		
+	}
+
+}
