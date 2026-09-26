738535884c3642def058006b098960d4e2359c0c
Akshay-0614 <doddiakshaykumar2@gmail.com>
2026-09-26 10:33:16 +0530

    Added Java program demonstrating multiple static methods

---
+++ 01-Java-Basics/MethodProgram.java
@@ -0,0 +1,218 @@
+//Write a Java program using multiple static methods to display personal and educational details.
+
+package com.task;
+
+public class MethodProgram {
+
+	// ==================== PERSONAL INFORMATION ====================
+
+	static void fullName() {
+		System.out.println("My Name is : Akshay kumar");
+		System.out.println(" ");
+
+	}
+
+	static void details() {
+		System.out.println("I'm from  Mancherial ");
+		System.out.println(" ");
+
+	}
+
+	static void currentDetails() {
+		System.out.println("I'm Currently Learning Java Full Stack With Gen AI in Vcube");
+		System.out.println(" ");
+
+	}
+
+	// ==================== PERSONAL DETAILS ====================
+
+	static void getHeight() {
+		System.out.println("Coming to my personal details ");
+		System.out.println("My Height is : 5.4");
+
+	}
+
+	static void getWeight() {
+
+		System.out.println("My Weight is : 59kg ");
+		System.out.println(" ");
+	}
+
+	// ==================== SSC & INTERMEDIATE DETAILS ====================
+
+	static void getSscMarks() {
+		System.out.println("I Completed My SSC in Ravindra High School in 2019");
+		System.out.println("My SSC CGPA is : 8.8");
+		System.out.println(" ");
+
+	}
+
+	static void getInterMarks() {
+		System.out.println("I completed my Intermediate in Prathibha junior college");
+		System.out.println("My Intermediate Marks is : 690");
+		System.out.println(" ");
+
+	}
+
+	// ==================== DEGREE DETAILS ====================
+
+	static void getDegreesem1() {
+		System.out.println("I completed My graduation in C.V.Raman Degree and P.G. College ");
+		System.out.println("My First semester CGPA is : 7.76");
+	}
+
+	static void getDegreesem2() {
+		System.out.println("My Second semester CGPA is : 7.7 ");
+	}
+
+	static void getDegreesem3() {
+		System.out.println("My Third semester CGPA is : 7.3");
+	}
+
+	static void getDegreesem4() {
+		System.out.println("My Fourth semester CGPA is : 7.9");
+
+	}
+
+	static void getDegreesem5() {
+		System.out.println("My Fifth semester CGPA is : 7.8");
+	}
+
+	static void getDegreesem6() {
+		System.out.println("My Sixth semester CGPA is : 7.98");
+
+	}
+
+	static void getDegreeoverallresult() {
+		System.out.println("My Degree Overall CGPA result is : 7.74");
+		System.out.println(" ");
+
+	}
+
+	// ==================== POST GRADUATION DETAILS ====================
+
+	static void getPgCollege() {
+		System.out.println("I Completed My Post Graduation in University Arts And Science College ");
+	}
+
+	static void getPgsem1() {
+		System.out.println("My First Semester Result is :.7.89 ");
+	}
+
+	static void getPgsem2() {
+		System.out.println("My Second Semester Result is : 7.77");
+
+	}
+
+	static void getPgsem3() {
+		System.out.println("My Third Semester Result is : 7.9");
+
+	}
+
+	static void getPgsem4() {
+		System.out.println("My Fourth Semester Result is : 8.54");
+	}
+
+	static void getPgoverallresult() {
+		System.out.println("My PG Overall Result is : 8.03 ");
+		System.out.println(" ");
+
+	}
+
+	// ==================== PERSONAL INTERESTS ====================
+
+	static void getHobbies() {
+		System.out.println("My Hobbies are Playing Cricket and Listening Songs");
+		System.out.println(" ");
+
+	}
+
+	static void getStrength() {
+		System.out.println("My Strength is i can easily adapt the situation");
+		System.out.println(" ");
+
+	}
+
+	static void getGoal() {
+		System.out.println("My goal is to see myself as Software Developer");
+		System.out.println(" ");
+
+	}
+
+	static void getDream() {
+		System.out.println("I want to visit all the top devotional places or temples in india");
+		System.out.println(" ");
+
+	}
+
+	static void getFavouritecolour() {
+		System.out.println("My Favourite Colour is : Blue");
+		System.out.println(" ");
+
+	}
+
+	static void getLuckyNumber() {
+		System.out.println("My Lucky Number is : 5");
+		System.out.println(" ");
+
+	}
+
+	static void getBestMoments() {
+		System.out.println("My Best Moments are placed in my Degree life ");
+		System.out.println(" ");
+
+	}
+
+	static void getBestPlace() {
+		System.out.println("My Best Place is : Kondagattu Anjaneya Swamy Temple");
+		System.out.println(" ");
+
+	}
+
+	static void getFavouriteTeacher() {
+		System.out.println("My Favourite Teacher in my School is Sadanandan sir");
+		System.out.println(" ");
+
+	}
+
+	// ==================== CONCLUSION ====================
+
+	static void getconclusion() {
+		System.out.println("This is My Info .....");
+	}
+
+	public static void main(String[] args) {
+
+		fullName();
+		details();
+		currentDetails();
+		getHeight();
+		getWeight();
+		getSscMarks();
+		getInterMarks();
+		getDegreesem1();
+		getDegreesem2();
+		getDegreesem3();
+		getDegreesem4();
+		getDegreesem5();
+		getDegreesem6();
+		getDegreeoverallresult();
+		getPgCollege();
+		getPgsem1();
+		getPgsem2();
+		getPgsem3();
+		getPgsem4();
+		getPgoverallresult();
+		getHobbies();
+		getStrength();
+		getGoal();
+		getDream();
+		getFavouritecolour();
+		getLuckyNumber();
+		getBestMoments();
+		getBestPlace();
+		getFavouriteTeacher();
+		getconclusion();
+	}
+
+}
