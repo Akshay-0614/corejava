f62b47c4d8008802dcca9fd3b76b74b788b2c95f
Akshay-0614 <doddiakshaykumar2@gmail.com>
2026-09-26 10:37:44 +0530

    Added Employee object and variable program

--- 01-Java-Basics/Employee.java
+++ 01-Java-Basics/Employee.java
@@ -1,46 +1,42 @@
-//Write a Java program to declare employee details using different data types and display them using a method.
-
-package corejava;
-
-// This program demonstrates : The Declaration of variables 
-// Here i used different primitive data types
+package com.task;
 
 public class Employee {
-	byte Employee_Id = 1;
-	int Employee_Age = 23;
-	int Employee_salary = 50000;
-	Float Employee_Grade = 98.9f; // Float is used because the grade contains decimal value
-	int Yearsofexperience = 15;
-	Boolean Employee_Active = true;// Boolean represents the employee's current status
-	int Numberofleaves;
-	long Employee_phno = 934606952l;
-
-	void display() {
-
-		// Deducting one leave before displaying the current balance
-		Numberofleaves--;
-
-		System.out.println("Employee Id  : " + Employee_Id);
-		System.out.println("Employee Age : " + Employee_Age);
-		System.out.println("Employee Salary : " + Employee_salary);
-		System.out.println("Employee Grade : " + Employee_Grade);
-		System.out.println("Employee Year of Experience : " + Yearsofexperience);
-		System.out.println("Employee Status : " + Employee_Active);
-		System.out.println("Employee Leaves : " + Numberofleaves);
-		System.out.println("Employee Phone Number : " + Employee_phno);
-	}
-
+	
+	// Stores the unique ID of the employee
+	int EmpId;
+	
+	// Stores the Name of the employee
+	String EmpName;
+	
+	// Stores the salary of the employee
+	Double EmpSalary;
+	
 	public static void main(String[] args) {
 
-		// Creating an Employee object using the default constructor
-		Employee e1 = new Employee();
-
-		// Assigning the initial leave balance to the employee
-		e1.Numberofleaves = 5;
-
-		// Calling display() to process and print employee details
-		e1.display();
-
+		//Creating an Object of the employee class
+		Employee e = new Employee ();
+		
+		// Assigning employee ID to the object
+		e.EmpId = 7737;
+		
+		// Assigning employee name to the object
+		e.EmpName="Akshay";
+		
+		// Assigning employee salary to the object
+		e.EmpSalary=50000d;
+		
+		// Displaying employee details
+		System.out.println("Employee Details ");
+		System.out.println(" ");
+		
+		// Displaying employee id
+		System.out.println("Employee_Id : "+e.EmpId);
+		
+		// Displaying employee name
+		System.out.println("Name of the Employee : "+e.EmpName);
+		
+		// Displaying employee salary
+		System.out.println("Employee's Salary : "+e.EmpSalary);
 	}
 
 }
